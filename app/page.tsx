a78c98568a48f1ad98bcfe8fed79cad788264ed7
LauraBLei <laura.blei@outlook.com>
2025-12-14 17:40:55 +0100

    added basic start menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome homepage</h1>
+    <div className="relative h-full flex items-center justify-center">
+      <div className="overflow-hidden w-full h-full absolute z-0">
+        <Image
+          src="/bg-images/outsideTavern.png"
+          alt=""
+          fill
+          className="object-cover"
+        />
+      </div>
+      <div className="flex flex-col text-white items-center justify-center w-full h-full z-10 gap-10 bg-black/30 ">
+        <h1 className="text-6xl md:text-9xl">Tomplania</h1>
+        <a className="text-4xl md:text-6xl cursor-pointer">Start</a>
+      </div>
     </div>
   );
 }
